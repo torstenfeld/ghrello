75a443f186ccd76ca681cc18d3a590f68f95de8d
Torsten Feld <torsten@torsten-feld.de>
2012-10-04 08:37:25 +0200

    added config with account for testing

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 
 <?php
 
-	$key = 'key'; // fill with your public key
-	$secret = 'secret'; // fill with your secret (OAuth) key
-	$url = "https://api.trello.com/1/"; // trello api URL
+	$username = 'torstenfeldtest';
+	$apiKey = '84ea5756b34394e1f4b1e13298f8eb32'; // key from test account
+	$secret = 'c16e034d0a6be202eec4b4b381aa7c50a8cb11820f059dd1f9f590556ef964f5'; // secret from test account
+	// $url = "https://api.trello.com/1/"; // trello api URL
 
 
 

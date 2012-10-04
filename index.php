66cb0c2496525f86c9d5d6f765dae842f1fa2d01
Torsten Feld <torsten@torsten-feld.de>
2012-10-04 09:12:32 +0200

    parsing returned data of object into array

--- index.php
+++ index.php
@@ -6,11 +6,36 @@
 
 	$h = NEW TrelloClient($username, $apiKey);
 	//~ $test = $h->getMember();
-	$test = $h->listBoardCards('506b1cbc632aaf5560154a80');
+	$data = $h->listBoardCards('506b1cbc632aaf5560154a80');
+	//~ $test = $h->decode($data);
+
+
+	//~ echo $h->data;
+
+	//~ echo '<pre>';
+	//~ echo var_dump($data);
+	//~ echo '</pre>';
+
+	$array = (array) $data;
+
+	echo '<br/><br/><br/>';
+
+	echo $array['data'];
+
+	echo '<br/><br/><br/>';
 
 	echo '<pre>';
-	echo print_r($test);
+	echo print_r(json_decode($array['data']));
 	echo '</pre>';
+
+	//~ echo '<pre>';
+	//~ echo print_r($array);
+	//~ echo '</pre>';
+
+
+	unset($h);
+
+
 
 	//~ echo '<form action="index.php" method="post">';
 	//~ echo '<textarea id="payload" name="payload" class="element textarea medium"></textarea>';

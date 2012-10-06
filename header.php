6f76a75794e479be95a2e21a62b83ef05f375da9
Torsten Feld <torsten@torsten-feld.de>
2012-10-06 11:14:42 +0200

    php variable $tr_boardid is stored to js variable boardid

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 			<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 			<script src="https://api.trello.com/1/client.js?key=' .$apiKey. '"></script>
                         <script src="js/trello.js"></script>
+                        <script>boardid = "' .$tr_boardid. '"</script>
 		</head>
 		<!-- header end -->';
 

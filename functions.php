6732b78cb7b53020c16675cb82e7c339cb2d9326
Torsten Feld <torsten@torsten-feld.de>
2012-10-07 18:03:08 +0200

    switched to ini base config and prepared functions.php / old config variables are still used, as ini values are stored into php variables

--- functions.php
+++ functions.php
@@ -5,6 +5,16 @@
         
         $commits = array();
         $cards = array();
+        $config_array = parse_ini_file("config/config.ini");
+        $apiKey = $config_array['trapikey']; // key from test account
+        $tr_boardid = $config_array['trboardid']; // trello board id
+        $gh_project = $config_array['ghproject']; // name of github project
+        
+        
+        
+        function _CheckConfigValues() {
+            // check for valid values in config variables - see card 27 (https://trello.com/card/check-for-valid-values-in-config-variables/506ac35636fa37ae13919ff8/27)
+        }
         
         function _PostCommentToCard() {
             global $cards;
@@ -14,6 +24,48 @@
                 echo 'TrelloCardComment("' .$card['id']. '", "' .$card['text']. '");';
             }
             echo '</script>';
+        }
+        
+        function write_ini_file($assoc_arr, $path="config/config.ini", $has_sections=FALSE) { 
+            $content = ""; 
+            if ($has_sections) { 
+                foreach ($assoc_arr as $key=>$elem) { 
+                    $content .= "[".$key."]\n"; 
+                    foreach ($elem as $key2=>$elem2) { 
+                        if(is_array($elem2)) 
+                        { 
+                            for($i=0;$i<count($elem2);$i++) 
+                            { 
+                                $content .= $key2."[] = \"".$elem2[$i]."\"\n"; 
+                            } 
+                        } 
+                        else if($elem2=="") $content .= $key2." = \n"; 
+                        else $content .= $key2." = \"".$elem2."\"\n"; 
+                    } 
+                } 
+            } 
+            else { 
+                foreach ($assoc_arr as $key=>$elem) { 
+                    if(is_array($elem)) 
+                    { 
+                        for($i=0;$i<count($elem);$i++) 
+                        { 
+                            $content .= $key."[] = \"".$elem[$i]."\"\n"; 
+                        } 
+                    } 
+                    else if($elem=="") $content .= $key." = \n"; 
+                    else $content .= $key." = \"".$elem."\"\n"; 
+                } 
+            } 
+
+            if (!$handle = fopen($path, 'w')) { 
+                return false; 
+            } 
+            if (!fwrite($handle, $content)) { 
+                return false; 
+            } 
+            fclose($handle); 
+            return true; 
         }
 
 	function _CreateCard() {
